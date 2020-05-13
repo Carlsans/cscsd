6d59b127938343cc7bbe1ff0400b802f214ea4f3
Carlsans <46719148+Carlsans@users.noreply.github.com>
2020-05-13 14:51:48 -0400

    Update covidstats.php

--- wordpress/covidstats.php
+++ wordpress/covidstats.php
@@ -8,7 +8,7 @@ Plugin Name: Covid-19 Statistics Displayer
 Plugin URI: https://moduloinfo.ca/wordpress/
 Description: This plugin allow you to display the latest covid statistics and previsions for the entire world using [covidstats] shortcode on the page where you want the stats to display.
 Author: Carl Sansfacon
-Version: 1.0
+Version: 1.1
 Author URI: https://moduloinfo.ca/
 */
 
